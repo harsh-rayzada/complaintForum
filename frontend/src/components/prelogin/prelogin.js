798baeb2097923c7fbefc1f4046d69ec9373c031
harsh <harsh.rayzada@neutrinos.co>
2018-08-23 18:59:45 +0530

    added fields in signup and login forms

---
+++ frontend/src/components/prelogin/prelogin.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import 'prelogin.css';
+
+class prelogin extends Component{
+    render(){
+        <div className="prelogin">
+            <div className="form-container">
+                <loginform/>
+            </div>
+        </div>
+    }
+}
